1b6993708ae42637fa019c06b75f6496f041d95c
HilaDo <HilaDoTheCool@outlook.com>
2024-05-07 02:12:50 +0300

    Added time slot data

--- src/schedule.h
+++ src/schedule.h
@@ -1,10 +1,12 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
-
+#include <QVector>
+#include "timeslot.h"
 class Schedule
 {
 public:
     Schedule();
+    QVector<TimeSlot> timeSlots;
 };
 
 #endif // SCHEDULE_H
--- src/timeslot.h
+++ src/timeslot.h
@@ -7,6 +7,13 @@ class TimeSlot
 {
 public:
     TimeSlot();
+    QString day;
+    int hour;
+    QString place;
+    short int sectionNum;
+    short int groupNum;
+    QString type[3] = {"Lecture","Tutorial","Lab"};
+    //course to be added
 };
 
 #endif // TIMESLOT_H
